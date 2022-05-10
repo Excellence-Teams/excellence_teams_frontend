71f7fa06fe2e53814a30ae7f721bfd0ddd98d7fb
Christoph Waffler <waffler.christoph@gmail.com>
2022-05-10 22:41:42 +0200

    fix code style

--- lib/ui/screens/search/search.screen.dart
+++ lib/ui/screens/search/search.screen.dart
@@ -7,7 +7,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Search"),
+      child: const Text("Search"),
       color: ETColors.limeGreen,
     );
   }
