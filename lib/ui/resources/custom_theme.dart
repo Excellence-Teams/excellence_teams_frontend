b358733226a9bbc40d2eaaadbc23989d0bcdce36
Christoph Waffler <waffler.christoph@gmail.com>
2022-05-27 01:01:18 +0200

    remove unnecessary params from the theme

--- lib/ui/resources/custom_theme.dart
+++ lib/ui/resources/custom_theme.dart
@@ -1,28 +1,14 @@
 import 'package:excellence_teams_frontend/ui/resources/colors.dart';
-import 'package:excellence_teams_frontend/ui/resources/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class ETTheme {
   static ThemeData get defaultTheme {
     return ThemeData(
-      primaryColor: ETColors.limeGreen,
+      primaryColor: ETColors.green,
       scaffoldBackgroundColor: ETColors.white,
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all(ETColors.white),
-          textStyle: MaterialStateProperty.all(
-            ETTextStyles.montSemiBold
-                .copyWith(color: ETColors.white, fontSize: 20),
-          ),
-        ),
-      ),
+      focusColor: ETColors.limeGreen,
+      hintColor: ETColors.grey,
       fontFamily: "Mont",
-      buttonTheme: ButtonThemeData(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        buttonColor: ETColors.green,
-      ),
     );
   }
 }
