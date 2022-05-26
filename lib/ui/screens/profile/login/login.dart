6962a94799b8071041f963a432c27df29eef7a31
Christoph Waffler <waffler.christoph@gmail.com>
2022-05-27 01:03:23 +0200

    refactor login screen with minor code changes

--- lib/ui/screens/profile/login/login.dart
+++ lib/ui/screens/profile/login/login.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
-
 import 'package:excellence_teams_frontend/ui/resources/colors.dart';
 import 'package:excellence_teams_frontend/ui/resources/text_styles.dart';
+import 'package:excellence_teams_frontend/ui/widgets/button.dart';
+import 'package:excellence_teams_frontend/ui/widgets/text.dart';
+import 'package:excellence_teams_frontend/ui/widgets/text_field.dart';
 
 import 'package:flutter/material.dart';
 
@@ -12,21 +13,20 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     const breakpoint = 1000;
-    const paneProportion = 50;
 
     if (breakpoint < size.width) {
       return Flex(
         direction: Axis.horizontal,
         children: const [
-          LoginLeftSide(paneProportion: paneProportion),
-          LoginRightSide(paneProportion: paneProportion),
+          LoginLeftSide(),
+          LoginRightSide(),
         ],
       );
     } else {
       return Flex(
         direction: Axis.horizontal,
         children: const [
-          LoginLeftSide(paneProportion: paneProportion),
+          LoginLeftSide(),
         ],
       );
     }
@@ -36,15 +36,11 @@ class LoginScreen extends StatelessWidget {
 class LoginRightSide extends StatelessWidget {
   const LoginRightSide({
     Key? key,
-    required this.paneProportion,
   }) : super(key: key);
-
-  final int paneProportion;
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
-      flex: 100 - paneProportion,
       child: Container(
         color: ETColors.grey,
         child: const Center(
@@ -58,145 +54,125 @@ class LoginRightSide extends StatelessWidget {
 class LoginLeftSide extends StatefulWidget {
   const LoginLeftSide({
     Key? key,
-    required this.paneProportion,
   }) : super(key: key);
-
-  final int paneProportion;
 
   @override
   State<LoginLeftSide> createState() => _LoginLeftSideState();
 }
 
 class _LoginLeftSideState extends State<LoginLeftSide> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
-        flex: widget.paneProportion,
         child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  const Text(
-                    "Welcome back",
-                    style: ETTextStyles.montTitle,
-                  ),
-                  const Text(
-                    "Welcome back! Please enter your details.",
-                    style: ETTextStyles.montBook,
-                  ),
-                  const SizedBox(height: 20),
-                  const SizedBox(
-                    width: 400,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green, width: 1),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: ETColors.grey, width: 1.0),
-                        ),
-                        hintText: 'Email',
-                        labelText: 'Email',
-                        suffixIcon: Icon(
-                          Icons.mail_outline,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 14),
-                  const SizedBox(
-                    width: 400,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green, width: 1),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: ETColors.grey, width: 1.0),
-                        ),
-                        hintText: 'Password',
-                        labelText: 'Password',
-                        suffixIcon: Icon(
-                          Icons.lock,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 14),
-                  SizedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Checkbox(
-                          value: isChecked,
-                          onChanged: (value) {
-                            setState(() {
-                              // TODO integrate viewmodel here
-                              // TODO use isChecked value
-                              isChecked = !isChecked;
-                            });
-                          },
-                        ),
-                        const SizedBox(
-                          child: Text("Remember me",
-                              style: ETTextStyles.montRegular),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.fromLTRB(165, 0, 0, 0),
-                          child: Text("Forgot password?",
-                              style: ETTextStyles.montRegular),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 14),
-                ]),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: ETColors.black,
-                        minimumSize: const Size(240, 40),
-                        maximumSize: const Size(240, 40),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6)),
-                      ),
-                      child:
-                          const Text('Sign in', style: ETTextStyles.montWhite),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 14),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: ETColors.green,
-                        minimumSize: const Size(240, 40),
-                        maximumSize: const Size(240, 40),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6)),
-                      ),
-                      child: const Text('Create a new Account',
-                          style: ETTextStyles.montWhite),
-                      onPressed: () {},
-                    ),
-                  ],
+                ETText(
+                  "Welcome back",
+                  style: ETTextStyles.montSemiBold.copyWith(fontSize: 32),
                 ),
+                const ETText(
+                  "Welcome back! Please enter your details.",
+                  style: ETTextStyles.montBook,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                    width: 400,
+                    child: ETTextField(
+                      controller: _emailController,
+                      hintText: 'Email',
+                      trailingIcon: const Icon(
+                        Icons.mail_outline,
+                        color: ETColors.grey,
+                      ),
+                    )),
+                const SizedBox(height: 14),
+                SizedBox(
+                    width: 400,
+                    child: ETTextField(
+                      controller: _passwordController,
+                      inputVisible: false,
+                      hintText: 'Password',
+                      trailingIcon: const Icon(
+                        Icons.lock,
+                        color: ETColors.grey,
+                      ),
+                    )),
+                const SizedBox(height: 14),
+                SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            // TODO integrate viewmodel here
+                            // TODO use isChecked value
+                            isChecked = !isChecked;
+                          });
+                        },
+                      ),
+                      const SizedBox(
+                        child: ETText(
+                          "Remember me",
+                          style: ETTextStyles.montRegular,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 165),
+                        child: Text(
+                          "Forgot password?",
+                          style: ETTextStyles.montRegular.copyWith(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 14),
               ],
             ),
           ],
-        ));
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ETButton(
+                  label: 'sign in',
+                  onClick: () {
+                    // TODO add click logic
+                  },
+                ),
+                const SizedBox(height: 14),
+                ETButton(
+                  backgroundColor: ETColors.green,
+                  label: 'create a new account',
+                  onClick: () {
+                    // TODO add click logic
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
